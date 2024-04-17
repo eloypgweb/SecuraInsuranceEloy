20bbedd11a5d46463efc0133d4d5cad6ec84aca7
eloy6lega <eloy.perez@campusfp.es>
2024-04-17 16:23:17 +0200

    Footer terminado con icons + entendimiento de la tabla

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import { bootstrapTelephone } from '@ng-icons/bootstrap-icons';
+import { bootstrapEnvelope } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [
+    provideIcons({ featherAirplay, bootstrapTelephone, bootstrapEnvelope }),
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
