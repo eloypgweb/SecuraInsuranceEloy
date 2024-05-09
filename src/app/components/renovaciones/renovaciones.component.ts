d4cdfd807011af76fac4517a300d6095ebfa065f
eloy6lega <eloy.perez@campusfp.es>
2024-05-09 15:03:18 +0200

    Filtrado hecho, faltan chips

--- src/app/components/renovaciones/renovaciones.component.ts
+++ src/app/components/renovaciones/renovaciones.component.ts
@@ -28,7 +28,7 @@ import { ListRenovationsService } from '../../services/list-renovations.service'
 
 // Mis pipes
 import { MispipesModule } from '../../shared/pipes/mispipes.module';
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -42,6 +42,7 @@ import { FormsModule } from '@angular/forms';
     MatPaginatorModule,
     NgClass,
     NgIf,
+    NgFor,
     FormsModule,
   ],
   viewProviders: [
@@ -70,6 +71,16 @@ export class RenovacionesComponent implements AfterViewInit {
     'state',
   ];
   dataSource = new MatTableDataSource<any>();
+
+  uniquePolicyNumbers: string[] = []; // Inicializa uniquePolicyNumbers como un arreglo vacío
+
+  // Declaraciones para filtros
+  selectedPolicyNumber: string | undefined; // Propiedad para almacenar el número de póliza seleccionado
+  riskName: string = ''; // Inicializa riskName como una cadena vacía
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+  amount: number | undefined;
+  state: string | undefined;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -103,6 +114,16 @@ export class RenovacionesComponent implements AfterViewInit {
     this.listRenovationsService.listRenovations.subscribe((data: any) => {
       this.dataSource.data = data;
     });
+
+    // Obtener los datos de la tabla
+    const tableData = this.dataSource.data;
+
+    // Obtener todos los números de póliza únicos
+    const policyNumbersSet = new Set<string>();
+    tableData.forEach((row) => {
+      policyNumbersSet.add(row.nPolicy);
+    });
+    this.uniquePolicyNumbers = Array.from(policyNumbersSet);
   }
 
   orderType: 'asc' | 'desc' = 'asc';
@@ -130,7 +151,95 @@ export class RenovacionesComponent implements AfterViewInit {
     this.isFilterMenuOpen = false;
   }
 
+  // Para almacenar los filtros aplicados
+  appliedFilters: { label: string; value: any }[] = [];
+
   applyFilters() {
-    console.log('Aplicando filtros');
+    let filteredData = this.listRenovationsService.listRenovations.value; // Accede a los datos cargados por el servicio
+    // Obtenemos el valor seleccionado del número de póliza
+    const selectedNumber = this.selectedPolicyNumber;
+
+    // Filtrar los datos según el número de póliza seleccionado
+    if (this.selectedPolicyNumber) {
+      this.dataSource.filter = this.selectedPolicyNumber.trim().toLowerCase();
+    }
+    if (this.riskName) {
+      filteredData = filteredData.filter((item: any) =>
+        item.riskName.toLowerCase().includes(this.riskName.toLowerCase())
+      );
+    }
+    if (this.startDate && this.endDate) {
+      // Verifica que tanto startDate como endDate tengan valores asignados
+      filteredData = filteredData.filter(
+        (item: any) =>
+          item.contractDate >= this.startDate! &&
+          item.contractDate <= this.endDate!
+      );
+    }
+
+    if (this.amount) {
+      filteredData = filteredData.filter(
+        (item: any) => item.amount === this.amount
+      );
+    }
+    if (this.state) {
+      filteredData = filteredData.filter(
+        (item: any) => item.state === this.state
+      );
+    }
+
+    this.dataSource.data = filteredData;
+    // Llamada a la función para generar chips
+    this.generateFilterChips();
+    this.closeFilterMenu();
+  }
+
+  // Método para generar chips de filtro
+  generateFilterChips() {
+    this.appliedFilters = [];
+
+    if (this.selectedPolicyNumber) {
+      this.appliedFilters.push({
+        label: 'Nº de Póliza',
+        value: this.selectedPolicyNumber,
+      });
+    }
+
+    if (this.riskName) {
+      this.appliedFilters.push({
+        label: 'Nombre del riesgo',
+        value: this.riskName,
+      });
+    }
+
+    if (this.startDate && this.endDate) {
+      this.appliedFilters.push({
+        label: 'Fecha de validez',
+        value: `${this.startDate} - ${this.endDate}`,
+      });
+    }
+
+    if (this.amount) {
+      this.appliedFilters.push({ label: 'Importe', value: this.amount });
+    }
+
+    if (this.state) {
+      this.appliedFilters.push({ label: 'Estado', value: this.state });
+    }
+  }
+
+  // Método para eliminar un chip de filtro y actualizar la tabla
+  removeFilterChip(filter: any) {
+    // console.log('removeFilterChip' + filter);
+    // Encuentra el índice del filtro en el array appliedFilters
+    const index = this.appliedFilters.indexOf(filter);
+
+    // Si se encuentra el filtro, elimínalo del array
+    if (index >= 0) {
+      this.appliedFilters.splice(index, 1);
+
+      // Aplica los filtros nuevamente para actualizar la tabla
+      this.applyFilters();
+    }
   }
 }
